e58bc0bf77ba4c61780cbe8f6dd295b9a7981190
ddlyRabbit <akabhinav32@gmail.com>
2026-04-14 09:55:05 +0000

    feat(train): tune feet_air_time thresholds to 0.15-0.45

--- biped_env_cfg.py
+++ biped_env_cfg.py
@@ -710,8 +710,8 @@ class RewardsCfg:
         params={
             "command_name": "base_velocity",
             "sensor_cfg": SceneEntityCfg("contact_forces", body_names="foot_6061.*"),
-            "threshold_min": 0.25,
-            "threshold_max": 0.55,
+            "threshold_min": 0.15,
+            "threshold_max": 0.45,
         },
     )
     feet_slide = RewTerm(
