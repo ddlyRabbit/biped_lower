5cd21cfc91772b1d14fba40aeedfae2dedd9b7b4
ddlyRabbit <akabhinav32@gmail.com>
2026-04-22 22:23:35 +0000

    V126: double action_rate_l2 (-0.50)

--- biped_env_cfg.py
+++ biped_env_cfg.py
@@ -769,7 +769,7 @@ class RewardsCfg:
         func=base_mdp.joint_torques_l2,
         weight=-1e-05,
     )
-    action_rate_l2 = RewTerm(func=base_mdp.action_rate_l2, weight=-0.25)
+    action_rate_l2 = RewTerm(func=base_mdp.action_rate_l2, weight=-0.50)
     feet_air_time = RewTerm(
         func="biped_env_cfg:feet_air_time_adaptive_berkeley",
         weight=10.0,
