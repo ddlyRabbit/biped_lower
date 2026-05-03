ce13340e741ce1a3215069129fe3b8cce1e75dac
ddlyRabbit <akabhinav32@gmail.com>
2026-05-03 15:01:43 +0000

    feat(config): V137 - reduce delay to 1-7 steps, resume from V136 model_200

--- biped_train_rsl.py
+++ biped_train_rsl.py
@@ -116,7 +116,7 @@ def main():
     else:
         env_cfg = BipedFlatEnvCfg()
         env_id = "Biped-Flat-v0"
-        experiment = "biped_flat_v136"
+        experiment = "biped_flat_v137"
 
     env_cfg.scene.num_envs = args_cli.num_envs
     env_cfg.seed = args_cli.seed
