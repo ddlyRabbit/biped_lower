db5b98e72ca650258aa9bd1cfddc8d7f4fe6c9ba
ddlyRabbit <akabhinav32@gmail.com>
2026-06-13 19:20:55 +0000

    tune: set push_force_levels curriculum to start at iteration 200

--- biped_env_cfg.py
+++ biped_env_cfg.py
@@ -1006,7 +1006,7 @@ class CurriculumsCfg:
             "term_name": "push_robot",
             "max_velocity": [0.4, 0.4],     # max push 0.4 m/s
             "interval": 200 * 24,
-            "starting_step": 3000 * 24,     # start after 1000 iterations
+            "starting_step": 200 * 24,      # start after 200 iterations
         },
     )
     command_vel = CurrTerm(
